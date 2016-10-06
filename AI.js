8b8d318f9c7f5320a61888dfd0f05f5f837bf779
Lazerbeak12345 <s9125@s.loganschools.org>
2016-10-06 16:40:33 -0600

    Add prototypes

--- AI.js
+++ AI.js
@@ -10,5 +10,32 @@ function strongEval(str,globalObj) {
 	console.log(str);
 	return (new Function("window","with(window) {"+str+"}"))(obj);
 };
-function Ai(name,file/*opt*/) {
+function Ai(name,obj/*opt*/) {
+	/*
+	There are 2 ways two run this function
+	
+		var myAi= new Ai("My AI's name");//1st
+		var mySecondAi= new Ai("My 2nd AI's name",{
+			//values here
+		});//2nd
+	*/
+	if  (arguments.length<1) throw "The Ai constructor requires 1-2 values passed";
+	obj=obj||{};//prevent bugs
+	this.name=name||"AIR bot";
+	//add more props as build continues
+	
+	for (var i in obj) {
+		this[i]=obj[i];//overwrite with obj
+	}
+	
+	if (typeof this.name!="string") throw "The name for your Ai bot must be a string";
 };
+Ai.prototype.reactTo=function(input,from) {
+	/*
+	How to use this function:
+	
+		var myAi= new Ai("My AI's name");
+		myAi.reactTo("input"//usually somthing like "say(\"Lorem ipsum dolor sit amet.\");" or "moveTo(12,34);"
+		)
+	*/
+}
