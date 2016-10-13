369048e45ee0a923aff46ab0c491808297517df8
Lazerbeak12345 <s9125@s.loganschools.org>
2016-10-13 08:48:12 -0600

    fix "this" bug
    strong eval could read the global namespace with the this operator. Now the this operator returns globalObj

--- AI.js
+++ AI.js
@@ -3,12 +3,15 @@ function strongEval(str,globalObj) {
 	for (var index in window) {//overwrite all of the items in the global namespace (not actually changing them)
 		obj[index]=undefined;
 	}
+	for (var index in this) {//overwrite all of the items in the global-ish namespace (not actually changing them)
+		obj[index]=undefined;
+	}
 	for (var index in globalObj) {//copy over properties from passed object
 		if (globalObj.hasOwnProperty(index)) obj[index]=globalObj[index];
 	}
 	str=str.replace(/{\s*$/,"").replace(/^[^{]*}/,"");
 	//console.log(str);
-	return (new Function("window","with(window) {"+str+"}"))(obj);
+	return (new Function("window","with(window) {"+str+"}")).call(globalObj,obj);
 };
 function Ai(name,obj/*opt*/) {
 	/*
