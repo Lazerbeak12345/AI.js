70c5bb59739269f818fe83fb66e460c72dc4ceca
Lazerbeak12345 <s9125@s.loganschools.org>
2016-11-09 09:21:32 -0700

    Update Gruntfile.js
    removed jshint request until I can figure out how to make it not cancel builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,6 @@ module.exports = function(grunt) {
   });
  
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', 'jshint');
+  //grunt.registerTask('default', 'jshint');
  
 };
