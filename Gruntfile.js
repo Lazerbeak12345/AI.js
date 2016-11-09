b5e6ac0b5c884fd2941f42961ef1cd691760de59
Lazerbeak12345 <s9125@s.loganschools.org>
2016-11-09 09:45:15 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     }
   });
  
-  //grunt.loadNpmTasks('grunt-contrib-jshint');
-  //grunt.registerTask('default', 'jshint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('default', 'jshint');
  
 };
