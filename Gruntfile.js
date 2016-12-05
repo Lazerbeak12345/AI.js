f1cc79aaf1c4a03389681199b84f4e321fe48f43
Lazerbeak12345 <s9125@s.loganschools.org>
2016-12-05 12:13:29 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,17 @@ module.exports = function(grunt) {
 		jshint: {
 			all: ['Gruntfile.js', 'AI.js', 'strongEval.js']
 		},
-		min: {// see http://stackoverflow.com/a/13749226
+		/*min: {// see http://stackoverflow.com/a/13749226
 			js: {
 				src: 'AI.js',
 				dest: 'AI.min.js'
 			}
-		},
+		},*/
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-min');
-	grunt.registerTask('default', ['jshint','min']);
+	//grunt.loadNpmTasks('grunt-min');
+	grunt.registerTask('default', ['jshint'/*,'min'*/]);
 	
 	
 };
