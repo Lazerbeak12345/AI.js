dc538dff2c6560210f77ac9f8ee6b94cbcf9df95
Lazerbeak12345 <s9125@s.loganschools.org>
2016-12-13 11:12:02 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-var Ai=require("./AI.js");
+	var Ai=require("./AI.js");
 var bot=new Ai("Andy");
 
 module.exports = function(grunt) {
@@ -6,12 +6,19 @@ module.exports = function(grunt) {
 		jshint: {
 			all: ['Gruntfile.js', 'AI.js', 'strongEval.js']
 		},
-	});
-	
+		bootlint: {
+			options: {
+				stoponerror: false,
+				relaxerror: []
+			},
+			files: ['**.html']
+		}
+	});	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-bootlint');
 	grunt.registerTask('Talk to AI', 'Preforms a status check for the AI by asking the bot how it is doing.', function() {
 		grunt.log.writeln('{ "How are you?"');
 		grunt.log.writeln('} "'+bot.reactTo("How are you?")+'"');
 	});
-	grunt.registerTask('default', ['jshint','Talk to AI']);
+	grunt.registerTask('default', ['jshint','Talk to AI','bootlint']);
 };
