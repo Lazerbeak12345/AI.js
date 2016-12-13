48fd1b8cd01e7339985fcc97095bd062e4b556bd
Lazerbeak12345 <s9125@s.loganschools.org>
2016-12-13 10:34:58 -0700

    Athsetical modifications to "Talk to AI" task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,21 +6,13 @@ module.exports = function(grunt) {
 		jshint: {
 			all: ['Gruntfile.js', 'AI.js', 'strongEval.js']
 		},
-		/*uglify:{
-			my_target: {
-				files: {
-					'AI.min.js': ['AI.js'],
-				}
-			}
-		},*/
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	//grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('Talk to Ai', 'Preforms a status check for the AI', function() {
-		grunt.log.writeln('} "How are you?"');
-		grunt.log.writeln('} "'+bot.reactTo("How are you?")+'"');
+	grunt.registerTask('Talk to AI', 'Preforms a status check for the AI by asking the bot how it is doing.', function() {
+		grunt.log.writeln('(How are you?)>');
+		grunt.log.writeln('<('+bot.reactTo("How are you?")+')');
 	});
-	grunt.registerTask('default', ['jshint','Talk to Ai']);
+	grunt.registerTask('default', ['jshint','Talk to AI']);
 		
 };
